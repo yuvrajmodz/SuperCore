5418a0cab1afb1feb362cd7535fb81f902972f9e
yuvrajmodz <ys8619050@gmail.com>
2025-10-14 16:27:55 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,21 @@
+# SuperCore
+
+**Letest Version:** 1.1  
+**Developer:** @NacDevs  
+**Git Repo:** [GitHub](https://github.com/yuvrajmodz/SuperHex)
+
+SuperCore is a Python CLI tool to easily create and start Supervisor-managed processes in Linux.
+
+## Installation & Usage
+
+```bash
+pip install supercore
+supercore <command>
+```
+
+## Optional installation
+
+```bash
+sudo apt update -y
+sudo apt install supervisor -y
+```
---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="supercore",
+    version="1.2",
+    author="@NacDevs",
+    author_email="yuvrajmodz@gmail.com",
+    description="Simple CLI to create supervisor processes easily",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/yuvrajmodz/SuperCore",
+    packages=find_packages(),
+    python_requires='>=3.8',
+    entry_points={
+        'console_scripts': [
+            'supercore=supercore.cli:main',
+        ],
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: POSIX :: Linux",
+    ],
+)
+
